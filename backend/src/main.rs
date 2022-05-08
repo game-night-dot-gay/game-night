b86103e86d99dea6d3a650d93998b855af632d37
Amy Keibler <amelia.keibler@gmail.com>
2022-05-08 15:33:51 -0400

    Add initial tracing
    - use tracing_subscriber's EnvFilter to pull logging configuration from the
      RUST_LOG environment variable
      - This defaults to no output
    - add tower-http to get request related trace events for the service
    - we'll add tracing-opentelemetry for observability, but this should be enough
      for starters

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -2,18 +2,26 @@ use crate::config::AppConfig;
 use axum::{response::Json, routing::get, Router};
 use serde_json::json;
 use std::net::SocketAddr;
+use tracing_subscriber::{fmt, prelude::*, EnvFilter};
 
 mod config;
 
 #[tokio::main]
 async fn main() -> color_eyre::Result<()> {
     color_eyre::install()?;
+    tracing_subscriber::registry()
+        .with(fmt::layer())
+        .with(EnvFilter::from_default_env())
+        .init();
 
     let config = AppConfig::intialize()?;
 
-    let app = Router::new().route("/", get(|| async { Json(json!({"data": 42})) }));
+    let app = Router::new()
+        .route("/", get(|| async { Json(json!({"data": 42})) }))
+        .layer(tower_http::trace::TraceLayer::new_for_http());
 
     let addr = SocketAddr::try_from(([0, 0, 0, 0], config.port))?;
+    tracing::info!("Listening on {addr}");
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await?;
