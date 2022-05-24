d42af289bd061ae4ba989152734d4036e808aa84
Amy Keibler <3483663+amy-keibler@users.noreply.github.com>
2022-05-24 03:03:42 +0000

    Add Initial Logo (#82)
    * Add Initial Logo
    
    - Implement simple buttons to toggle different flags
    
    * Manage yarn.lock via yarn instead of npm

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
+    <object type="image/svg+xml" class="logo-image" data="/img/logo.svg">
+      <img src="/img/logo.svg" />
+    </object>
+    <div>
+      <button @click="selectGay">Select Gay Pride Flag</button>
+      <button @click="selectTrans">Select Trans Pride Flag</button>
+    </div>
+
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
   </div>
 </template>
@@ -9,10 +16,37 @@
 import { defineComponent } from "vue";
 import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 
+function selectFlag(flagClass: string) {
+  const logos = Array.from(document.getElementsByClassName("logo-image"));
+  logos.forEach((logo) => {
+    if (logo instanceof HTMLObjectElement) {
+      const logoDocument = logo.getSVGDocument();
+      const flags = logoDocument?.querySelectorAll(".flags > g");
+      flags?.forEach((flag) => {
+        if (flag.classList.contains(flagClass)) {
+          flag.removeAttribute("display");
+        } else {
+          flag.setAttribute("display", "none");
+        }
+      });
+    } else {
+      console.log("Error: element with class 'logo-image' that is not a flag");
+    }
+  });
+}
+
 export default defineComponent({
   name: "HomeView",
   components: {
     HelloWorld,
+  },
+  methods: {
+    selectGay() {
+      selectFlag("gay-flag");
+    },
+    selectTrans() {
+      selectFlag("trans-flag");
+    },
   },
 });
 </script>
