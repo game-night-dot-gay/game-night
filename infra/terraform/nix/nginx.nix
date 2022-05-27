cc7271dee98bd2262d187b7c09d2ac9155d29c88
Allie Sierra <85902135+allisonsierra@users.noreply.github.com>
2022-05-27 19:22:47 -0400

    Make Rainbow Button Work
    Change is in the Packer file but needs ro be here also.

--- infra/terraform/nix/nginx.nix
+++ infra/terraform/nix/nginx.nix
@@ -45,7 +45,7 @@
     add_header 'Referrer-Policy' 'origin-when-cross-origin';
 
     # Disable embedding as a frame
-    add_header X-Frame-Options DENY;
+    add_header X-Frame-Options SAMEORIGIN;
 
     # Prevent injection of code in other mime types (XSS Attacks)
     add_header X-Content-Type-Options nosniff;
