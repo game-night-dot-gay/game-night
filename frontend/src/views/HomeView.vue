917363af556f663402bf1b387c3bbfece7212790
Amelia Keibler <amelia.keibler@gmail.com>
2022-05-27 15:16:13 -0400

    Add logos for other pride flags

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -4,8 +4,14 @@
       <img src="/img/logo.svg" />
     </object>
     <div>
-      <button @click="selectGay">Select Gay Pride Flag</button>
-      <button @click="selectTrans">Select Trans Pride Flag</button>
+      <button @click="selectFlag('gay')">Select Gay Pride Flag</button>
+      <button @click="selectFlag('trans')">Select Trans Pride Flag</button>
+      <button @click="selectFlag('non-binary')">
+        Select Non-Binary Pride Flag
+      </button>
+      <button @click="selectFlag('lesbian')">Select Lesbian Pride Flag</button>
+      <button @click="selectFlag('philly')">Select Philly Pride Flag</button>
+      <button @click="selectFlag('agender')">Select Agender Pride Flag</button>
     </div>
 
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
@@ -16,36 +22,35 @@
 import { defineComponent } from "vue";
 import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 
-function selectFlag(flagClass: string) {
-  const logos = Array.from(document.getElementsByClassName("logo-image"));
-  logos.forEach((logo) => {
-    if (logo instanceof HTMLObjectElement) {
-      const logoDocument = logo.getSVGDocument();
-      const flags = logoDocument?.querySelectorAll(".flags > g");
-      flags?.forEach((flag) => {
-        if (flag.classList.contains(flagClass)) {
-          flag.removeAttribute("display");
-        } else {
-          flag.setAttribute("display", "none");
-        }
-      });
-    } else {
-      console.log("Error: element with class 'logo-image' that is not a flag");
-    }
-  });
-}
-
 export default defineComponent({
   name: "HomeView",
   components: {
     HelloWorld,
   },
   methods: {
-    selectGay() {
-      selectFlag("gay-flag");
-    },
-    selectTrans() {
-      selectFlag("trans-flag");
+    selectFlag(flagClass: string) {
+      // set the user's preferred flag as a cookie
+      document.cookie = "flag=" + flagClass;
+
+      // activate the flag in any "logo-image" <object> tags
+      const logos = Array.from(document.getElementsByClassName("logo-image"));
+      logos.forEach((logo) => {
+        if (logo instanceof HTMLObjectElement) {
+          const logoDocument = logo.getSVGDocument();
+          const flags = logoDocument?.querySelectorAll(".flags > g");
+          flags?.forEach((flag) => {
+            if (flag.classList.contains(flagClass + "-flag")) {
+              flag.removeAttribute("display");
+            } else {
+              flag.setAttribute("display", "none");
+            }
+          });
+        } else {
+          console.log(
+            "Error: element with class 'logo-image' that is not a flag"
+          );
+        }
+      });
     },
   },
 });
