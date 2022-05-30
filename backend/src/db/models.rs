5ac1481eeacd326e4c4f9c5f75eec7148cb1b201
Amy Keibler <amelia.keibler@gmail.com>
2022-05-30 18:59:41 -0400

    Add input sanitization
    - Demonstrate this via a query to insert a user into the database

--- backend/src/db/models.rs
+++ backend/src/db/models.rs
@@ -1,9 +1,15 @@
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
 #[derive(Debug, Serialize)]
 pub struct User {
     pub user_id: Uuid,
+    pub display_name: String,
+    pub pronouns: String,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct InsertionUser {
     pub display_name: String,
     pub pronouns: String,
 }
