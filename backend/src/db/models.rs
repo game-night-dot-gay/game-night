eca51c52b56e570a93e032c05ba96524a53838c5
Amelia Keibler <amelia.keibler@gmail.com>
2022-05-31 12:43:48 -0400

    Add user details
    Use the definition in "documentation/design/USER_MANAGEMENT.md" for the fields

--- backend/src/db/models.rs
+++ backend/src/db/models.rs
@@ -6,10 +6,15 @@ pub struct User {
     pub user_id: Uuid,
     pub display_name: String,
     pub pronouns: String,
+    pub email: String,
+    pub dietary_needs: Option<String>,
+    pub is_admin: bool,
 }
 
 #[derive(Debug, Deserialize)]
 pub struct InsertionUser {
     pub display_name: String,
     pub pronouns: String,
+    pub email: String,
+    pub dietary_needs: Option<String>,
 }
