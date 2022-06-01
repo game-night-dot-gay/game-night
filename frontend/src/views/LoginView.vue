cc95f588ade6bb8f9c73d0ed6b8df3effc873113
Amelia Keibler <amelia.keibler@gmail.com>
2022-05-31 21:24:19 -0400

    Add initial login page
    - Create a /login route and a corresponding view
    - Extract the flag into a FlagLogo component
    - Delete some of the builtin VueJS starter template

---
+++ frontend/src/views/LoginView.vue
@@ -0,0 +1,57 @@
+<template>
+  <FlagLogo />
+  <div class="login">
+    <label>Email:</label>
+    <input v-model="email" />
+    <button id="request-login-button" @click="requestLogin">Login</button>
+    <div id="submission-message" :class="submissionClass">
+      Check your email for the login link.
+    </div>
+  </div>
+</template>
+
+<style>
+#submission-message {
+  display: none;
+}
+
+#submission-message.display {
+  display: block;
+}
+</style>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import FlagLogo from "@/components/FlagLogo.vue"; // @ is an alias to /src
+
+export default defineComponent({
+  name: "LoginView",
+  data() {
+    return {
+      email: "",
+      submitted: false,
+    };
+  },
+  computed: {
+    submissionClass() {
+      return {
+        display: this.submitted,
+      };
+    },
+  },
+  components: {
+    FlagLogo,
+  },
+  methods: {
+    requestLogin() {
+      console.log(this.email);
+
+      const requestLoginButton = document.getElementById(
+        "request-login-button"
+      );
+      requestLoginButton?.setAttribute("disabled", "");
+      this.submitted = true;
+    },
+  },
+});
+</script>
